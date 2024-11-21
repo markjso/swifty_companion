6bf51c95007b7e39d16aaaf06f27b8d8a59e28a0
Jo Marks <97271827+markjso@users.noreply.github.com>
2024-11-21 11:33:46 +1030

    Update Profile.js

--- views/Profile.js
+++ views/Profile.js
@@ -1,86 +1,121 @@
-import { Text, View, StyleSheet, Image, TouchableOpacity, Alert, ScrollView  } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import * as Progress from 'react-native-progress';
+import { Text, View, StyleSheet, Image, TouchableOpacity, ScrollView  } from 'react-native';
+
+const getProjectColor = (project) => {
+  if (project["validated?"] === true) {
+    return "green";
+  }
+  if (project["validated?"] === false) {
+    return "red";
+  }
+  if (project["final_mark"] === 0) {
+    return "red";
+  }
+  switch (project.status) {
+    case "in_progress":
+      return "orange";
+    case "finished":
+      return "green";
+    case "parent":
+      return "red";
+    default:
+      return "white";
+  }
+};
 
 export default function ProfileScreen({ route, navigation }) {
-  const { user } = route.params;
-
-  const displaySkills = (skills) => {
-    return skills
-      .sort((a, b) => b.level - a.level)
-      .map((skill) => (
-        <View key={skill.id} style={styles.row}>
-          <View style={styles.skill_level}>
-            <Text style={styles.text}>{skill.name}: {skill.level}</Text>
-            <View style={styles.progressBar}>
-              <Progress.Bar 
-                progress={skill.level / 10}
-                width={200} 
-                color='blue'
-                borderWidth={0}
-              />
-             </View> 
+  const { user, coalition } = route.params;
+ 
+  const listProjects = (projects_users) => {
+    return projects_users
+    .filter((project_user) => {
+      return (
+        project_user.status === 'in_progress' ||
+     project_user.status == 'finished'
+     );
+    })
+      .map((project_user) => (
+        <View key={project_user.id} style={styles.projectContainer}>
+          <View style={styles.projectRow}>
+            <Text style={[styles.projectName, { color: getProjectColor(project_user) }]}>
+              {project_user.project.name}
+            </Text>
+            <Text style={styles.projectScore}> 
+            {project_user.final_mark !== null ? `Grade: ${project_user.final_mark}` : project_user.status}
+            </Text>
           </View>
         </View>
-      ));
+      ))
   };
 
   const cursus = user.cursus_users && user.cursus_users.length > 1 ? user.cursus_users[1] : null;
   const skills = cursus ? cursus.skills : [];
+  const projects_users = user.projects_users;
+
+  const dynamicLabelStyle = {
+    ...styles.label,
+    color: coalition?.color || 'white', 
+  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <Icon name="arrow-back" size={30} color="white" />
+      <TouchableOpacity onPress={() => navigation.navigate('Skills', {user})}>
+        <Text style={styles.link}>Skills</Text>
       </TouchableOpacity>
       <Image style={styles.userImage} source={{ uri: user.profilePicture }} />
       <View style={styles.row}>
-        <Text style={styles.label}>Name</Text>
+        <Text style={dynamicLabelStyle}>Name</Text>
         <Text style={styles.value}>{user.firstName} {user.lastName}</Text>
       </View>
       <View style={styles.row}>
-        <Text style={styles.label}>Campus</Text>
+        <Text style={dynamicLabelStyle}>Campus</Text>
         <Text style={styles.value}>{user.campus}</Text>
       </View>
       <View style={styles.row}>
-        <Text style={styles.label}>Wallet</Text>
+        <Text style={dynamicLabelStyle}>Wallet</Text>
         <Text style={styles.value}>{user.wallet} ₳</Text>
       </View>
       <View style={styles.row}>
-        <Text style={styles.label}>Evaluation Points</Text>
+        <Text style={dynamicLabelStyle}>Evaluation Points</Text>
         <Text style={styles.value}>{user.evalPoints}</Text>
       </View>
       <ScrollView>
-        <View style={styles.skillContainer}>
-          <Text style={styles.text}>Skills</Text>
-          {skills.length > 0 ? (
-            displaySkills(skills)
-          ) : (
-            <Text style={styles.text}>No skills found</Text>
-          )}
+        <View style={[styles.projectContainer, { alignItems: 'center' }]}>
+          <Text style={styles.title}>Projects</Text>
+        {projects_users.length > 0 ? (
+          listProjects(projects_users)
+        ) : (
+          <Text style={styles.text}>No projects found</Text>
+        )}
         </View>
-      </ScrollView>  
+      </ScrollView>   
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#1e1d1d',
-    paddingTop: 50,
+    backgroundColor: '#f6f6f6',
+    paddingTop: 30,
   },
   userImage: {
-    height: 100,
-    width: 100,
+    height: 120,
+    width: 120,
     borderRadius: 85,
     marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 20,
   },
   text: {
     fontSize: 20,
     color: 'white',
-    marginBottom: 20,
+    marginTop: 10,
     textAlign: 'center',
   },
   row: {
@@ -92,34 +127,45 @@ const styles = StyleSheet.create({
   label: {
     flex: 1,
     textAlign: 'left',
-    fontSize: 20,
-    color: 'gold',
-    paddingLeft: 30,
+    fontSize: 19,
+    paddingLeft: 20,
+    fontWeight: 'bold',
   },
   value: {
     flex: 1,
     textAlign: 'right',
     fontSize: 20,
-    color: 'white',
-    paddingRight: 30,
+    color: 'black',
+    paddingRight: 20,
   },
-  backButton: {
-    position: 'absolute',
-    top: 40,
-    left: 20,
+  link: {
+    fontSize: 18,
+    color: 'blue',
+    textDecorationLine: 'underline',
+    textAlign: 'right',
   },
-  skillContainer: {
-    alignItems: 'left',
+  projectContainer: {
+    alignItems: 'flex-start',
     justifyContent: 'center',
     backgroundColor: '#1e1d1d',
-    paddingTop: 20,
+    paddingHorizontal: 10,
+    marginTop: 20,
   },
-  skill_level: {
-    flexDirection: 'row',
-    textAlign: 'left',
+  projectRow: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    width: '100%',
   },
-  progressBar: {
-    marginTop: 5,
-    width: '80%',
+  projectName: {
+    flex: 1, 
+    textAlign: 'left', 
+    fontSize: 20,
+    color: 'white',
+  },
+  projectScore: {
+    textAlign: 'right', 
+    fontSize: 20,
+    color: 'white',
   },
 });
