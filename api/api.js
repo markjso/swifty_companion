3ba415715a9af3a87ccb303adbbe3b182e12b311
Jo Marks <97271827+markjso@users.noreply.github.com>
2024-11-25 12:13:48 +1030

    Ready for development

--- api/api.js
+++ api/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alert } from 'react-native';
+import * as SecureStore from "expo-secure-store";
+import { CLIENT_ID, CLIENT_SECRET } from "@env";
 
-const INTRA_CLIENT_ID = "u-s4t2ud-f38935f9680154b391f5f4f4540a392546731f574fbd3502235239a7f754be59";
-const INTRA_CLIENT_SECRET = "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";
 const TOKEN_URL = "https://api.intra.42.fr/oauth/token";
 const USER_URL = "https://api.intra.42.fr/v2/users/";
 
 export const getAccessToken = async () => {
 	const response = await axios.post(TOKEN_URL, {
 		grant_type: "client_credentials",
-		client_id: INTRA_CLIENT_ID,
-		client_secret: INTRA_CLIENT_SECRET,
+		client_id: CLIENT_ID,
+		client_secret: CLIENT_SECRET,
 	});
 
   	return response.data.access_token;
@@ -32,7 +31,6 @@ export const searchUsers = async (login) => {
   }
 
   try {
-    Alert.alert('Info', `Searching for user: ${login}`);
     const response = await axios.get(`${USER_URL}/${login}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -40,18 +38,24 @@ export const searchUsers = async (login) => {
     });
 
    if (response.status === 200) {
+     const data = response.data;
       const user = {
                     login: data.login,
+                    user_id: data.id,
                     firstName: data.first_name,
                     lastName: data.last_name,
                     email: data.email,
                     profilePicture: data.image.link,
                     wallet: data.wallet,
-      }
+                    evalPoints: data.correction_point,
+                    campus: data.campus[0].name,
+                    cursus_users: data.cursus_users,
+                    projects_users: data.projects_users,
+      };
     return user;
     }
   } catch (error) {
-    console.error('Error fetching users', error);
+    console.error('User does not exist', error);
 
     // Check if the error is a 401 Unauthorized, indicating token expiration
     if (error.response && error.response.status === 401) {
@@ -66,7 +70,6 @@ export const searchUsers = async (login) => {
             Authorization: `Bearer ${accessToken}`,
           },
         });
-        Alert.alert('Info', 'User data fetched successfully after retry');
         return retryResponse.data;
       } catch (retryError) {
         console.error('Retry failed', retryError);
@@ -74,8 +77,24 @@ export const searchUsers = async (login) => {
       }
     }
 
-    Alert.alert('Error', 'Error fetching users');
-    throw new Error('Failed to fetch user data');
+    throw new Error('User does not exist');
   }
 };
 
+export const getCoalition = async (login) => {
+  if (!accessToken) {
+    Alert.alert('Info', 'Initializing access token');
+    await initializeAccessToken();
+  }
+
+ try {
+    const response = await axios.get(`${USER_URL}/${login}/coalitions`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      });
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching user coalition");
+  }
+}
